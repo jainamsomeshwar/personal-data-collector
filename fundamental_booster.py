8fbae537dac2afa5db191ab1dd1080795476d39f
pinal <jainamsomeshwar@gmail.com>
2026-04-05 20:32:58 +0530

    Add files via upload

---
+++ fundamental_booster.py
@@ -0,0 +1,17 @@
+welcome to the interactive personal data collector!
+
+
+please enter your name: jainam someshwar 
+please enter your age: 20
+please enter your height in feet: 5.6
+please enter your fav number: 7
+
+Thank you! here is the information we collector!
+
+Name: jainam (type: <class 'str'>), memory address: 140432678901456
+age: 20 (type: <class 'int'>), memory address: 140432678901488
+height: 5.6 (type: <class 'float'>), memory address: 140432678901520
+fav_number: 7 (type: <class 'int'>), memory address: 140432678901552
+your birth year is: 2004
+ 
+Thank you for using the interactive personal data collector have a great day!
